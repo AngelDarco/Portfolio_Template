8646e260383937fc919841c4c77c5027aedb68ae
angeldarco <angel92-eme@hotmail.com>
2024-10-08 10:52:22 -0500

    :fire: code refactored

--- app.js
+++
@@ -1,7 +0,0 @@
-import writerEffect from './writerEffect.js';
-    document.addEventListener('DOMContentLoaded', ()=>{
-   const ground = document.querySelector('.content1')
-   const text = 'testing';
-    writerEffect(ground, text)
-})
-
