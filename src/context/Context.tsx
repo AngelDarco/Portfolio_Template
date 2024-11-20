e806c5d0b129893eaa02741dd8106cb84d0e54a2
angeldarco <angel92-eme@hotmail.com>
2024-11-20 05:32:20 -0500

    :sparkles: useContext added

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,28 +1,37 @@
 import { createContext, useState } from "react";
 
-const defaultValue = [
-  { img: "/image_1.png" },
-  { img: "/image_2.png" },
-  { img: "image_4.png" },
-  { img: "/image_5.png" },
-  { img: "/image_6.png" },
-  { img: "/image_7.png" },
-  { img: "image_8.png" },
-  { img: "/image_1.png" },
-  { img: "/image_2.png" },
-  { img: "image_4.png" },
-  { img: "/image_5.png" },
-  { img: "/image_6.png" },
-  { img: "/image_7.png" },
-  { img: "image_8.png" },
+interface Data {
+  img: string;
+  categorie: string;
+}
+
+export const defaultValue: Data[] = [
+  { img: "/image_1.png", categorie: "bags" },
+  { img: "/image_2.png", categorie: "clothes" },
+  { img: "image_4.png", categorie: "clothes" },
+  { img: "/image_5.png", categorie: "caps" },
+  { img: "/image_6.png", categorie: "cups" },
+  { img: "/image_7.png", categorie: "clothes" },
+  { img: "image_8.png", categorie: "cups" },
+  { img: "/image_1.png", categorie: "bags" },
+  { img: "/image_2.png", categorie: "clothes" },
+  { img: "image_4.png", categorie: "clothes" },
+  { img: "/image_5.png", categorie: "caps" },
+  { img: "/image_6.png", categorie: "cups" },
+  { img: "/image_7.png", categorie: "clothes" },
+  { img: "image_8.png", categorie: "cups" },
 ];
 
-export const context = createContext(defaultValue);
+interface Context {
+  data: typeof defaultValue;
+  setData?: React.Dispatch<React.SetStateAction<Data[]>>;
+}
 
-export default function StoreProvider({ children }) {
+export const context = createContext<Context>({ data: defaultValue });
+
+export default function StoreProvider({ children }: { children: JSX.Element }) {
   const [data, setData] = useState(defaultValue);
-  console.log(data);
   return (
-    <context.Provider value={[data, setData]}>{children}</context.Provider>
+    <context.Provider value={{ data, setData }}>{children}</context.Provider>
   );
 }
