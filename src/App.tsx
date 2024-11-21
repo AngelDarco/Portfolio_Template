3dcd0200a475f585fa84434f3b01919b94209f44
angeldarco <angel92-eme@hotmail.com>
2024-11-21 17:11:54 -0500

    :sparkles: routes created

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,30 @@ import Footer from "./components/Footer";
 import Main from "./components/Main";
 import CardsContainer from "./components/CardsContainer";
 import Carousel from "./components/Carousel";
-import StoreProvider from "./context/Context";
+import { Route, Switch } from "wouter";
+import Admin from "./components/Admin";
 
 function App() {
   return (
-    <div className="flex items-center min-w-96  w-full max-w-5xl  flex-col">
-      <StoreProvider>
-        <Header />
-        <Main />
-        <Carousel />
-        <CardsContainer />
-        <Footer />
-      </StoreProvider>
+    <div className="flex items-center min-w-96  w-full max-w-5xl min-h-dvh flex-col">
+      <Header />
+      <Switch>
+        <Route path={"/"} component={Home} />
+        <Route path={"/admin"} component={Admin} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
+
+const Home = () => {
+  return (
+    <>
+      <Main />
+      <Carousel />
+      <CardsContainer />
+    </>
+  );
+};
 
 export default App;
