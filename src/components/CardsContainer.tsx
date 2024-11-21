24ca2fd8e32f549f728b914e3692b6a389c9b811
angeldarco <angel92-eme@hotmail.com>
2024-11-21 17:14:41 -0500

    :racehorse: card container updated to use contex values

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -1,27 +1,13 @@
+import { useContext } from "react";
+import { context } from "../context/Context";
 import Card from "./Card";
 export default function CardsContainer() {
-  const items = [
-    { img: "/image_1.png" },
-    { img: "/image_2.png" },
-    { img: "image_4.png" },
-    { img: "/image_5.png" },
-    { img: "/image_6.png" },
-    { img: "/image_7.png" },
-    { img: "image_8.png" },
-    { img: "/image_1.png" },
-    { img: "/image_2.png" },
-    { img: "image_4.png" },
-    { img: "/image_5.png" },
-    { img: "/image_6.png" },
-    { img: "/image_7.png" },
-    { img: "image_8.png" },
-  ];
+  const { data } = useContext(context);
 
   return (
-    <main className="auto flex w-full flex-wrap items-center justify-center gap-4 p-4">
-      {items.map((item, index) => (
-        <Card key={index} img={item.img} icon />
-      ))}
+    <main className="flex w-full min-h-dvh flex-wrap justify-center  gap-4 p-4">
+      {data &&
+        data.map((item, index) => <Card key={index} img={item.img} icon />)}
     </main>
   );
 }
