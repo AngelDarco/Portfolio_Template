47f554977e07e2ca583c401e383176ef2c2d3605
angeldarco <angel92-eme@hotmail.com>
2024-11-23 03:14:40 -0500

    :hammer: header links updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,6 +38,7 @@ export default function Header() {
   useEffect(() => {
     window.addEventListener("scroll", ScrollListener);
     return () => window.removeEventListener("scroll", ScrollListener);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
@@ -61,15 +62,15 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-      <div className="w-full text-white">
-        <Link href="admin">
+      <div className="w-full flex items-center">
+        <Link href="admin/add">
           <svg
             stroke="currentColor"
             fill="currentColor"
             strokeWidth="0"
             viewBox="0 0 24 24"
-            height="34px"
-            width="34px"
+            height="24px"
+            width="24px"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path d="M12 14V22H4C4 17.5817 7.58172 14 12 14ZM12 13C8.685 13 6 10.315 6 7C6 3.685 8.685 1 12 1C15.315 1 18 3.685 18 7C18 10.315 15.315 13 12 13ZM21 17H22V22H14V17H15V16C15 14.3431 16.3431 13 18 13C19.6569 13 21 14.3431 21 16V17ZM19 17V16C19 15.4477 18.5523 15 18 15C17.4477 15 17 15.4477 17 16V17H19Z"></path>
