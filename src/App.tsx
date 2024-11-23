fdef13aefd0b84847c34947b0435aac2f8f373ba
angeldarco <angel92-eme@hotmail.com>
2024-11-23 03:18:10 -0500

    :racehorse: Main routes updated to accept params in the url

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,15 @@ import Footer from "./components/Footer";
 import Main from "./components/Main";
 import CardsContainer from "./components/CardsContainer";
 import Carousel from "./components/Carousel";
-import { Route, Switch } from "wouter";
+import { Route } from "wouter";
 import Admin from "./components/Admin";
 
 function App() {
   return (
     <div className="flex items-center min-w-96  w-full max-w-5xl min-h-dvh flex-col">
       <Header />
-      <Switch>
-        <Route path={"/"} component={Home} />
-        <Route path={"/admin"} component={Admin} />
-      </Switch>
+      <Route path={"/"} component={Home} />
+      <Route path={"/admin/:uid"} component={Admin} />
       <Footer />
     </div>
   );
