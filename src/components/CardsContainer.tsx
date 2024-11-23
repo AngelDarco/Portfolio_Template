9e0717bb6a75bfb1de93393e1d24a389b8302561
angeldarco <angel92-eme@hotmail.com>
2024-11-23 03:19:02 -0500

    :racehorse: cars container added uid param

--- src/components/CardsContainer.tsx
+++ src/components/CardsContainer.tsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { context } from "../context/Context";
 import Card from "./Card";
+
 export default function CardsContainer() {
   const { data } = useContext(context);
 
   return (
     <main className="flex w-full min-h-dvh flex-wrap justify-center  gap-4 p-4">
       {data &&
-        data.map((item, index) => <Card key={index} img={item.img} icon />)}
+        data.map((item, index) => (
+          <Card key={index} img={item.img} icon admin uid={item.uid} />
+        ))}
     </main>
   );
 }
