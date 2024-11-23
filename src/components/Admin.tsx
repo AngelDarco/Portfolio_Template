317dcab49190dfd4cfd05b24c02811ff250117aa
angeldarco <angel92-eme@hotmail.com>
2024-11-23 03:22:44 -0500

    :racehorse: form validation, access uid and ux added to the  Admin component

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,62 +1,94 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import Database from "../firebase/functions";
+import { Data } from "../@types";
+import { useParams } from "wouter";
+import Notify from "../utils/notify";
+import { ToastContainer } from "react-toastify";
 
 export default function Admin() {
   const db = new Database();
+  const { get, shower } = db.store();
 
-  const [img, setImg] = useState("image_02.png");
+  const uid = useParams().uid;
 
-  const { add, get } = db.store();
+  const defaultLocalImg = "image_02.png";
+  const defaultDbImg = "/tunning-store/girl.jpg";
+
+  const [img, setImg] = useState(defaultLocalImg);
+
   useEffect(() => {
-    get("/tunning-store/girl.jpg", setImg);
+    get(defaultDbImg, setImg);
+    // eslint-disable-next-line
   }, []);
 
   const handlerFile = (file: ChangeEvent) => {
-    const inputElement = file.target as HTMLInputElement;
+    shower(file, setImg);
+  };
 
-    if (inputElement.files && inputElement.files[0]) {
-      const fileData = inputElement.files[0];
-      const reader = new FileReader();
-      reader.readAsDataURL(fileData);
-      reader.onload = () => {
-        if (typeof reader.result === "string") setImg(reader.result);
-      };
-    }
+  function formValidation(e: FormEvent) {
+    const form = new FormData(e.target as HTMLFormElement);
+    const formData = Object.fromEntries(form.entries());
+
+    const data: Data = {};
+
+    const res = Object.entries(formData).map(([key, value]) => {
+      if (value === "" || (key === "image" && (value as File).name === ""))
+        return Notify.error(`${key} is required`);
+
+      data[key] = value;
+    });
+
+    if (res.length === Object.values(data).length) return data;
+  }
+
+  const handlerUpdate = (e: FormEvent) => {
+    e.preventDefault();
+    const data = formValidation(e);
+    if (!data) return;
+
+    console.log(data);
+    // if (typeof uid === "string" && uid !== "add") {
+    // }
   };
 
   return (
-    <main className="flex flex-col gap-4 min-h-dvh">
-      <h1 className="text-3xl text-white">Admin board</h1>
+    <section className="flex w-full flex-col gap-4  h-screen border">
+      <h1 className="text-3xl text-center text-white">Admin board</h1>
 
-      <div className="w-60">
-        <img className="w-full" src={img} alt="" />
-      </div>
+      <main className="flex h-full gap-4">
+        <div className="w-full">
+          <img className="w-auto" src={img} alt="" />
+        </div>
 
-      <div>
-        <input
-          type="file"
-          name="search"
-          accept="image/jpg"
-          onChange={handlerFile}
-        />
-      </div>
-
-      <form className="flex flex-col gap-4">
-        <label htmlFor="name">
-          <input type="text" name="name" id="name" placeholder="Name" />
-        </label>
-        <label htmlFor="email">
-          <input type="email" name="email" id="email" placeholder="Email" />
-        </label>
-        <label htmlFor="password">
+        <form
+          onSubmit={handlerUpdate}
+          className="flex flex-col h-full gap-4 w-full [&>label>input]:rounded-md [&>label>input]:p-1 [&>label]:h-9 [&>label>input]:h-full "
+        >
           <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Password"
+            type="file"
+            name="image"
+            accept="image/jpg"
+            onChange={handlerFile}
+            disabled={uid === "add" ? false : true}
           />
-        </label>
-      </form>
-    </main>
+          <label htmlFor="name">
+            <input type="text" name="name" id="name" placeholder="Name" />
+          </label>
+          <label htmlFor="email">
+            <input type="email" name="email" id="email" placeholder="Email" />
+          </label>
+          <label htmlFor="password">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+          </label>
+          <button>Update</button>
+        </form>
+      </main>
+      <ToastContainer />
+    </section>
   );
 }
