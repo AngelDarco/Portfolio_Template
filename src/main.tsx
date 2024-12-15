01e361ab75c2246ecfe277d732868df6fa2731b1
angeldarco <angel92-eme@hotmail.com>
2024-12-15 12:39:27 -0500

    :racehorse: provider context added

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import StoreProvider from "./context/Context";
+import StoreProvider from "./context/Provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
