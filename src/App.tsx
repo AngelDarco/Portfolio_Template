cafb7cf1474aaf51ed5469ca9621d8470b3d4fa7
angeldarco <angel92-eme@hotmail.com>
2024-12-15 12:38:28 -0500

    :racehorse: not found route added

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import Footer from "./components/Footer";
 import Main from "./components/Main";
 import CardsContainer from "./components/CardsContainer";
 import Carousel from "./components/Carousel";
-import { Route } from "wouter";
+import NotFound from "./components/NotFound";
+import { Route, Switch } from "wouter";
 import Admin from "./components/Admin";
 
 function App() {
   return (
-    <div className="flex items-center min-w-96  w-full max-w-5xl min-h-dvh flex-col">
+    <div className="flex items-center min-w-96 w-full max-w-5xl min-h-dvh gap-10 flex-col">
       <Header />
-      <Route path={"/"} component={Home} />
-      <Route path={"/admin/:section/:uid"} component={Admin} />
+      <Switch>
+        <Route path={"/"} component={Home} />
+        <Route path={"/admin/:section/:uid"} component={Admin} />
+        <Route component={NotFound} />
+      </Switch>
       <Footer />
     </div>
   );
