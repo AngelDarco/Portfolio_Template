fe09043cd4a9cc339eeceaa75615c7275cdc782e
angeldarco <angel92-eme@hotmail.com>
2024-12-17 15:51:06 -0500

    :racohorse:  new types added

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,5 +1,11 @@
-export type Data = {
+export type FormData = {
   uid: string;
   img: string | File;
+  categorie: string;
+};
+
+export type Data = {
+  uid: string;
+  img: string;
   categorie: string;
 };
