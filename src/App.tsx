49d1abc30d4dfe8890040a9a728c86a4d77f731a
angeldarco <angel92-eme@hotmail.com>
2024-12-17 15:54:41 -0500

    :racohorse:  new styles and private routes added

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,18 @@ import Carousel from "./components/Carousel";
 import NotFound from "./components/NotFound";
 import { Route, Switch } from "wouter";
 import Admin from "./components/Admin";
+import Login from "./components/Login";
 
 function App() {
+  const admin = false;
+
   return (
-    <div className="flex items-center min-w-96 w-full max-w-5xl min-h-dvh gap-10 flex-col">
+    <div className="flex items-center justify-center min-w-96 w-full max-w-5xl min-h-screen gap-10 flex-col relative h-auto">
       <Header />
       <Switch>
         <Route path={"/"} component={Home} />
-        <Route path={"/admin/:section/:uid"} component={Admin} />
+        <Route path={"/login"} component={Login} />
+        {admin && <Route path={"/admin/:section/:uid"} component={Admin} />}
         <Route component={NotFound} />
       </Switch>
       <Footer />
